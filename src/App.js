8c8a7a6281e324e5ea74287b1c4163df7b453ef2
Alexis Sanchez <alexis_sanchezv@hotmail.com>
2022-11-28 12:26:46 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+
+function App() {
+  
+  const handleDownloadVideo = async () => {
+    try {
+      const proxyurl = "https://shrouded-refuge-96344.herokuapp.com/";
+      const url = 'https://www.fincomun.com.mx/videos/VIDEO_NUEVA_NORMALIDAD.mp4';
+      //const videoRequest = new Request(url);
+      fetch(proxyurl + url)
+        .then(() => {
+          const link = document.createElement('a');
+          link.href = url;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+      <h1>DESCARGAR VIDEO</h1>
+        <button id='download' type='button' onClick={handleDownloadVideo}>DESCARGAR</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
