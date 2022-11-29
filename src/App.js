f91072e02c67d2b033dff2e84e873a0b77ac73ba
Alexis Sanchez <alexis_sanchezv@hotmail.com>
2022-11-28 19:40:34 -0600

    add tomls

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import './App.css';
-const cors = require('cors');
 
 function App() {
-  
   const handleDownloadVideo = async () => {
-    fetch(cors('https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'), {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/mp4',
-      },
-    })
-    .then((response) => response.blob())
-    .then((blob) => {
-      const url = window.URL.createObjectURL(
-        new Blob([blob]),
-      );
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute(
-        'download',
-        `FileName.mp4`,
-      );
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-    });
+    try {
+      const url = 'https://www.fincomun.com.mx/videos/VIDEO_NUEVA_NORMALIDAD.mp4';
+      //const videoRequest = new Request(url);
+      fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(
+          new Blob([blob]),
+        );
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute(
+          'download',
+          `FileName.mp4`,
+        );
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        });
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
     
     <div className="App">
